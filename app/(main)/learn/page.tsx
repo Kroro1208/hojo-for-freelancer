0eb34b86827cabec5ba5ebef653562d6b9182c6d
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-29 07:45:24 +0900

    learnページのヘッダー部分実装した

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,19 @@
+import { FeedWrapper } from "@/components/feedWrapper"
+import { StickyWrapper } from "@/components/stickyWrapper"
+import { Header } from "./header"
 
 const LearnPage = () => {
   return (
-    <div>
-      Learn Page
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+        <StickyWrapper>
+            Learn Page
+        </StickyWrapper>
+        <FeedWrapper>
+            <Header title="Header" />
+        </FeedWrapper>
     </div>
   )
 }
 
 export default LearnPage
+ 
