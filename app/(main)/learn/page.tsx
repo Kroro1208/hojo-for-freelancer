dfe1a4b849a7202bad6ebfeb7dbd48d525350321
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-29 20:34:04 +0900

    loading表示させた

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,18 @@
+import { redirect } from "next/navigation"
 import { FeedWrapper } from "@/components/feedWrapper"
 import { StickyWrapper } from "@/components/stickyWrapper"
 import { Header } from "./header"
 import { UserProgress } from "@/components/userProgress"
+import { getUserProgress } from "@/db/queries"
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+  const [ userProgress ] = await Promise.all([userProgressData]);
+
+  if(!userProgress || !userProgress.activeCourse) {
+    redirect("/course");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
         <StickyWrapper>
