34bb01c538c67bfd3023b8e92e70011efa11a88d
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-29 13:36:54 +0900

    learn pageのアイコンとレイアウト実装した

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,12 +1,18 @@
 import { FeedWrapper } from "@/components/feedWrapper"
 import { StickyWrapper } from "@/components/stickyWrapper"
 import { Header } from "./header"
+import { UserProgress } from "@/components/userProgress"
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
         <StickyWrapper>
-            Learn Page
+            <UserProgress
+                activeCourse={{title: "Business", imageSrc: "/businessman.svg"}}
+                hearts={5}
+                points={10}
+                hasActiveSubscriotion={false}
+            />
         </StickyWrapper>
         <FeedWrapper>
             <Header title="Header" />
