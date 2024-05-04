d81acb138c145f459e7a3008e6c12be3c89ed45f
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-04 22:32:32 +0900

    問題をDBから取得して表示させた

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -2,13 +2,15 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./header";
+import { QuestionBubble } from "./questionBubble";
+import { Challenge } from "./challenge";
 
 type Props = {
     initialLessonId: number;
     initialPercentage: number;
     initialHearts: number;
     initialLessonChallenges: (typeof challenges.$inferSelect & {
-        comleted: boolean;
+        completed: boolean;
         challengeOptions: typeof challengeOptions. $inferSelect[];
     })[];
     userSubscriptions: any;
@@ -21,8 +23,18 @@ export const Quiz = ({
     initialLessonChallenges,
     userSubscriptions
 }: Props) => {
-    const [ hearts, setHearts ] = useState(50 || initialHearts);
-    const [ percentage, setPercentage ] = useState(50 || initialPercentage);
+    const [ hearts, setHearts ] = useState(initialHearts);
+    const [ percentage, setPercentage ] = useState(initialPercentage);
+    const [challenges] = useState(initialLessonChallenges);
+    const [ activeIndex, setActiveIndex ] = useState(() => {
+        const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
+        return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+    });
+
+    const challenge = challenges[activeIndex];
+    const options = challenge?.challengeOptions ?? [];
+
+    const title = challenge.type === "ASSIST"  ? "最も適切な解を選択してください" : challenge.question;
 
   return (
     <>
@@ -31,6 +43,27 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscriptions?.isActive}
       />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+            <div className="lg:min-h-[350px] lg-w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+                <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+                    {title}
+                </h1>
+                <div>
+                    {challenge.type === "ASSIST" && (
+                        <QuestionBubble  question={challenge.question}/>
+                    )}
+                    <Challenge options={options}
+                    onSelect={()=>{}}
+                    status="none"
+                    selectedOption={undefined}
+                    disabled={false}
+                    type={challenge.type}
+                    />
+                </div>
+            </div>
+        </div>
+      </div>
     </>
   )
 }
