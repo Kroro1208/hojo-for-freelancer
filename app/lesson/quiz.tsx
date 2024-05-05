ab85afbb0cd9c8dbfe4c81c110e9cb23729ccf5b
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-06 07:29:36 +0900

    ボタンクリックして音声が流れるようにした

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./questionBubble";
 import { Challenge } from "./challenge";
+import Footer from "./footer";
 
 type Props = {
     initialLessonId: number;
@@ -31,8 +32,15 @@ export const Quiz = ({
         return uncompletedIndex === -1 ? 0 : uncompletedIndex;
     });
 
+    const [ selectedOption, setSelectedOption ] = useState<number>();
+    const [ status, setStatus ] = useState<"correct" | "wrong" | "none">("none");
+
     const challenge = challenges[activeIndex];
     const options = challenge?.challengeOptions ?? [];
+    const onSelect = (id: number) => {
+      if(status !== "none") return;
+      setSelectedOption(id);
+    };
 
     const title = challenge.type === "ASSIST"  ? "最も適切な解を選択してください" : challenge.question;
 
@@ -53,17 +61,23 @@ export const Quiz = ({
                     {challenge.type === "ASSIST" && (
                         <QuestionBubble  question={challenge.question}/>
                     )}
-                    <Challenge options={options}
-                    onSelect={()=>{}}
-                    status="none"
-                    selectedOption={undefined}
-                    disabled={false}
-                    type={challenge.type}
+                    <Challenge
+                      options={options}
+                      onSelect={onSelect}
+                      status={status}
+                      selectedOption={selectedOption}
+                      disabled={false}
+                      type={challenge.type}
                     />
                 </div>
             </div>
         </div>
       </div>
+      <Footer
+        disabled={!selectedOption}
+        status={status}
+        onCheck={()=>{}}
+      />
     </>
   )
 }
