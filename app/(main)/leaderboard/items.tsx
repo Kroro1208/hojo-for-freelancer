a7ddd758524907bd2a4bc2b7dee5ee1a6af6b5a4
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-14 15:24:28 +0900

    ユーザーのスコアでランキング表示できるようにした

---
+++ app/(main)/leaderboard/items.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { toast } from "sonner";
+import { refillHearts } from "@/actions/userProgress";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useTransition } from "react";
+import { createStripeUrl } from "@/actions/userSubscription";
+import { POINTS_TO_REFILL } from "@/constants";
+
+type Props = {
+    hearts: number;
+    points: number;
+    hasActiveSubscription: boolean;
+}
+
+const Items = ({
+    hearts,
+    points,
+    hasActiveSubscription
+}: Props) => {
+    const [pending, startTransition] = useTransition();
+    const onRefillHearts = () => {
+        if(pending || hearts === 5 || points < POINTS_TO_REFILL){
+            return;
+        }
+        startTransition(() => {
+            refillHearts().catch(() => toast.error('エラーが発生しました'))
+        });
+    }
+
+    const onUpgrade = () => {
+        startTransition(() => {
+            createStripeUrl().then((response) => {
+                if(response.data){
+                    window.location.href = response.data;
+                }
+            }).catch(() => toast.error('エラーが発生しました'));
+        });
+    }
+
+    return (
+    <ul className="w-full">
+        <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
+            <Image
+            src="/hearts.svg"
+            alt="Hearts"
+            height={30}
+            width={30}
+            />
+            <div className="flex-1">
+                <p className="text-neutral-700 text-base lg:text-lg font-bold">
+                    ポイントを使ってハートを補充
+                </p>
+            </div>
+            <Button
+            variant="secondary"
+            onClick={onRefillHearts}
+            disabled={pending || hearts === 5 || points < POINTS_TO_REFILL}
+            >
+                {hearts === 5 ? "補充する" :(
+                    <div className="flex items-center gap-2">
+                        <Image
+                        src="/points.svg"
+                        alt="Points"
+                        height={20}
+                        width={20}
+                        />
+                        <p>
+                            {POINTS_TO_REFILL}
+                        </p>
+                    </div>
+                )}
+            </Button>
+        </div>
+        <div className="flex items-center w-full p-4 pt-8 gap-x-4 border-t-2">
+            <Image
+            src="/love-heart.gif"
+            alt="HeartMove"
+            height={30}
+            width={30}
+            />
+            <div className="flex-1">
+                <p className="text-neutral-700 text-base lg:text-lg font-bold">
+                    ハートを永久補充する
+                </p>
+            </div>
+            <Button
+            variant="primary"
+            onClick={onUpgrade}
+            disabled={pending}
+            >
+                {hasActiveSubscription ? "設定" : "UPGRADE"}
+            </Button>
+        </div>
+    </ul>
+    )
+}
+
+export default Items
---
+++ app/(main)/leaderboard/page.tsx
@@ -0,0 +1,76 @@
+import { redirect } from "next/navigation";
+import { StickyWrapper } from "@/components/stickyWrapper"
+import { UserProgress } from "@/components/userProgress"
+import { getUserProgress, getUserSubscription } from "@/db/queries"
+import { FeedWrapper } from "@/components/feedWrapper";
+import Image from "next/image";
+import { getTopTenUsers } from '../../../db/queries';
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
+
+const LeaderBoardPage = async () => {
+    const userProgressData = getUserProgress();
+    const userSubscriptionData = getUserSubscription();
+    const usersRankingData = getTopTenUsers(); 
+
+    const [ userProgress, userSubscription, usersRanking ] = await Promise.all([
+        userProgressData, userSubscriptionData, usersRankingData
+    ]);
+
+    if(!userProgress || !userProgress.activeCourse) {
+        redirect("/course");
+    }
+    const isPro = !!userSubscription?.isActive;
+    return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+        activeCourse={userProgress.activeCourse}
+        hearts={userProgress.hearts}
+        points={userProgress.points}
+        hasActiveSubscriotion={isPro}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <div className="w-full flex flex-col items-center">
+            <Image
+            src="/medal.svg"
+            alt="LeaderBoard"
+            height={90}
+            width={90}
+            />
+            <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
+            あなたのスコア
+            </h1>
+            <p className="text-muted-foreground text-center text-lg mb-6">
+                コミュニティ内のあなたの順位
+            </p>
+            <Separator className="mb-4 h-0.5 rounded-full"/>
+            {usersRanking.map((userProgress, index) => {
+              return (
+                <div key={userProgress.userId}
+                className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
+                >
+                  <p className="font-bold text-lime-700 mr-4">{index + 1}</p>
+                  <Avatar className="bg-green-500 h-12 w-12 ml-3 mr-6">
+                    <AvatarImage
+                    src={userProgress.userImageSrc}
+                    className="object-cover"
+                    />
+                  </Avatar>
+                  <p className="font-bold text-neutral-800 flex-1">
+                    {userProgress.userName}
+                  </p>
+                  <p className="text-muted-foreground">
+                    {userProgress.points} XP
+                  </p>
+                </div>
+              )
+            })}
+        </div>
+      </FeedWrapper>
+    </div>
+  )
+}
+
+export default LeaderBoardPage
